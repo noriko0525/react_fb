9bc3d1ac687dd9f01e326414fce7e32eab789af5
EC2 Default User <ec2-user@ip-172-31-9-238.ec2.internal>
2021-04-06 12:00:31 +0000

    change

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCJJKC8wAKpS8nLpodnY6Be4D16IIIsm1Y",
+    authDomain: "react-firebase-3a776.firebaseapp.com",
+    projectId: "react-firebase-3a776",
+    storageBucket: "react-firebase-3a776.appspot.com",
+    messagingSenderId: "581801423222",
+    appId: "1:581801423222:web:3dccf3aadff394491f8a57"
+};
